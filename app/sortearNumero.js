61b9b9d5ed88ac06d82bc037822121fc20a5c0c2
Matheus Estevam <68551343+matheusenc@users.noreply.github.com>
2022-12-21 17:44:04 -0300

    Update sortearNumero.js

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,5 +1,5 @@
-const menorValor = 1 //altere o valore para mudar o range do Math.random()
-const maiorValor = 1000 //altere o valore para mudar o range do Math.random()
+const menorValor = 1 //altere o valor para mudar o range do Math.random()
+const maiorValor = 1000 //altere o valor para mudar o range do Math.random()
 const numeroSecreto = gerarNumeroAleatorio()
 
 function gerarNumeroAleatorio() {
